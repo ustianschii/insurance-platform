c14c9b8881d7895853fcccce74accbc1bdb75b47
Cristian <ustianschii@gmail.com>
2024-05-26 13:34:28 +0300

    avatars styles in review component refactored

--- src/components/Body/Reviews/Reviews.tsx
+++ src/components/Body/Reviews/Reviews.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { ReviewsWrapper } from "./reviewsStyles";
+import {
+  AvatarBig,
+  AvatarSmall,
+  QuoteIcon,
+  ReviewsBgRightWrapper,
+  ReviewsWrapper,
+} from "./reviewsStyles";
 import { Box, Typography, Divider, IconButton } from "@mui/material";
-import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
 import StarIcon from "@mui/icons-material/Star";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import reviewBlockBG from "../../../assets/reviews-block-bg.jpeg";
 import reviewBlockAvatar1 from "../../../assets/pic-user-1.webp";
 import reviewBlockAvatar2 from "../../../assets/pic-user-2.webp";
 import reviewBlockAvatar3 from "../../../assets/pic-user-3.webp";
@@ -35,18 +39,9 @@ const Reviews = () => {
         >
           Ce spun clienții noștri despre noi
         </Typography>
-        <Box sx={{ display: "flex", height: "50%", width: "100%" }}>
-          <FormatQuoteIcon
-            sx={{
-              height: "45px",
-              width: "45px",
-              color: "white",
-              marginRight: "25px",
-              backgroundColor: "#01ad40",
-              borderRadius: "5px",
-            }}
-          />
-          <Box sx={{ display: "block", width: "100%" }}>
+        <Box display={"flex"} height={"50%"}>
+          <QuoteIcon />
+          <Box display={"block"} width={"100%"}>
             <Typography>
               <Typography marginBottom={"10px"}>
                 Personal foarte prietenos. Mi-au pregatit asigurarea după apel
@@ -65,12 +60,7 @@ const Reviews = () => {
             <Typography marginTop={"10px"} fontWeight={"bold"}>
               Alexandru
             </Typography>
-            <Box
-              sx={{
-                color: "#fa7327",
-                margin: "5px 0 0 -5px",
-              }}
-            >
+            <Box color={"#fa7327"} m={"5px 0 0 -5px"}>
               <StarIcon />
               <StarIcon />
               <StarIcon />
@@ -80,12 +70,16 @@ const Reviews = () => {
           </Box>
         </Box>
 
-        <Box display={"flex"} sx={{ margin: "15px 0 0 58px" }}>
-          <IconButton sx={{ marginRight: "10px" }}>
+        <Box display={"flex"} m={"15px 0 0 58px"}>
+          <IconButton>
             <KeyboardArrowLeftIcon />
           </IconButton>
-          <Divider orientation="vertical" flexItem sx={{ opacity: "0.6" }} />
-          <IconButton sx={{ marginLeft: "10px" }}>
+          <Divider
+            orientation="vertical"
+            flexItem
+            sx={{ opacity: "0.6", margin: "0 10px 0 10px" }}
+          />
+          <IconButton>
             <KeyboardArrowRightIcon />
           </IconButton>
           <Typography
@@ -98,114 +92,63 @@ const Reviews = () => {
           </Typography>
         </Box>
       </Box>
-      <Box
-        sx={{
-          width: "600px",
-          backgroundImage: `url(${reviewBlockBG})`,
-          margin: "-10px 0 0 -60px",
-        }}
-      >
-        <Box
-          sx={{
-            width: "500px",
-            height: "100%",
-            marginLeft: "75px",
-          }}
-        >
-          <Box
+      <ReviewsBgRightWrapper>
+        <Box width={"500px"} height={"100%"} ml={"75px"}>
+          <AvatarSmall
+            top={"120px"}
             sx={{
-              position: "relative",
               backgroundImage: `url(${reviewBlockAvatar1})`,
-              height: "45px",
-              width: "45px",
-              borderRadius: "20px",
-              top: "120px",
             }}
           />
-          <Box
+          <AvatarSmall
+            top={"10px"}
+            left={"350px"}
             sx={{
-              position: "relative",
               backgroundImage: `url(${reviewBlockAvatar2})`,
-              backgroundSize: "cover",
-              height: "45px",
-              width: "45px",
-              borderRadius: "20px",
-              top: "10px",
-              left: "350px",
             }}
           />
-          <Box
+          <AvatarBig
+            top={"35px"}
+            left={"460px"}
             sx={{
-              position: "relative",
               backgroundImage: `url(${reviewBlockAvatar3})`,
-              backgroundSize: "cover",
-              height: "65px",
-              width: "65px",
-              borderRadius: "30px",
-              top: "35px",
-              left: "460px",
             }}
           />
-          <Box
+          <AvatarBig
+            top={"85px"}
+            left={"100px"}
             sx={{
-              position: "relative",
               backgroundImage: `url(${reviewBlockAvatar4})`,
-              backgroundSize: "cover",
-              height: "65px",
-              width: "65px",
-              borderRadius: "30px",
-              top: "85px",
-              left: "100px",
             }}
           />
-          <Box
+          <AvatarBig
+            left={"350px"}
+            top={"60px"}
             sx={{
-              position: "relative",
               backgroundImage: `url(${reviewBlockAvatar5})`,
-              backgroundSize: "cover",
-              height: "65px",
-              width: "65px",
-              borderRadius: "30px",
-              left: "350px",
-              top: "60px",
             }}
           />
-          <Box
+          <AvatarSmall
+            top={"70px"}
+            left={"50px"}
             sx={{
-              position: "relative",
               backgroundImage: `url(${reviewBlockAvatar6})`,
-              backgroundSize: "cover",
-              height: "45px",
-              width: "45px",
-              borderRadius: "20px",
-              top: " 70px",
-              left: "50px",
             }}
           />
-          <Box
+          <AvatarSmall
+            bottom={"-183px"}
             sx={{
-              position: "relative",
               backgroundImage: `url(${reviewBlockAvatar7})`,
-              backgroundSize: "cover",
-              height: "45px",
-              width: "45px",
-              borderRadius: "20px",
-              bottom: "-183px",
             }}
           />
-          <Box
+          <AvatarBig
+            left={"250px"}
             sx={{
-              position: "relative",
               backgroundImage: `url(${reviewBlockAvatar8})`,
-              backgroundSize: "cover",
-              height: "65px",
-              width: "65px",
-              borderRadius: "30px",
-              left: "250px",
             }}
           />
         </Box>
-      </Box>
+      </ReviewsBgRightWrapper>
     </ReviewsWrapper>
   );
 };
--- src/components/Body/Reviews/reviewsStyles.tsx
+++ src/components/Body/Reviews/reviewsStyles.tsx
@@ -1,5 +1,11 @@
-import { Container as MuiContainer, styled } from "@mui/material";
+import {
+  Container as MuiContainer,
+  Box as MuiBox,
+  styled,
+} from "@mui/material";
+import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
 import reviewsBlockBG from "../../../assets/bg-reviews-block2.png";
+import reviewsBlockBgRight from "../../../assets/reviews-block-bg.jpeg";
 
 export const ReviewsWrapper = styled(MuiContainer)(() => ({
   display: "flex",
@@ -9,4 +15,35 @@ export const ReviewsWrapper = styled(MuiContainer)(() => ({
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover",
   borderRadius: "15px",
+}));
+
+export const QuoteIcon = styled(FormatQuoteIcon)(() => ({
+  height: "45px",
+  width: "45px",
+  color: "white",
+  marginRight: "25px",
+  backgroundColor: "#01ad40",
+  borderRadius: "5px",
+}));
+
+export const ReviewsBgRightWrapper = styled(MuiBox)(() => ({
+  width: "600px",
+  backgroundImage: `url(${reviewsBlockBgRight})`,
+  margin: "-10px 0 0 -60px",
+}));
+
+export const AvatarSmall = styled(MuiBox)(() => ({
+  position: "relative",
+  backgroundSize: "cover",
+  height: "45px",
+  width: "45px",
+  borderRadius: "20px",
+}));
+
+export const AvatarBig = styled(MuiBox)(() => ({
+  position: "relative",
+  backgroundSize: "cover",
+  height: "65px",
+  width: "65px",
+  borderRadius: "30px",
 }));
