3275965e38ca7720396205846c3f545879c4639e
Cristian <ustianschii@gmail.com>
2024-05-26 12:19:25 +0300

    user avatars added on background of review component

--- src/components/Body/Reviews/Reviews.tsx
+++ src/components/Body/Reviews/Reviews.tsx
@@ -6,6 +6,14 @@ import StarIcon from "@mui/icons-material/Star";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import reviewBlockBG from "../../../assets/reviews-block-bg.jpeg";
+import reviewBlockAvatar1 from "../../../assets/pic-user-1.webp";
+import reviewBlockAvatar2 from "../../../assets/pic-user-2.webp";
+import reviewBlockAvatar3 from "../../../assets/pic-user-3.webp";
+import reviewBlockAvatar4 from "../../../assets/pic-user-4.webp";
+import reviewBlockAvatar5 from "../../../assets/pic-user-5.webp";
+import reviewBlockAvatar6 from "../../../assets/pic-user-6.webp";
+import reviewBlockAvatar7 from "../../../assets/pic-user-7.webp";
+import reviewBlockAvatar8 from "../../../assets/pic-user-8.webp";
 
 const Reviews = () => {
   return (
@@ -96,7 +104,108 @@ const Reviews = () => {
           backgroundImage: `url(${reviewBlockBG})`,
           margin: "-10px 0 0 -60px",
         }}
-      ></Box>
+      >
+        <Box
+          sx={{
+            width: "500px",
+            height: "100%",
+            marginLeft: "75px",
+          }}
+        >
+          <Box
+            sx={{
+              position: "relative",
+              backgroundImage: `url(${reviewBlockAvatar1})`,
+              height: "45px",
+              width: "45px",
+              borderRadius: "20px",
+              top: "120px",
+            }}
+          />
+          <Box
+            sx={{
+              position: "relative",
+              backgroundImage: `url(${reviewBlockAvatar2})`,
+              backgroundSize: "cover",
+              height: "45px",
+              width: "45px",
+              borderRadius: "20px",
+              top: "10px",
+              left: "350px",
+            }}
+          />
+          <Box
+            sx={{
+              position: "relative",
+              backgroundImage: `url(${reviewBlockAvatar3})`,
+              backgroundSize: "cover",
+              height: "65px",
+              width: "65px",
+              borderRadius: "30px",
+              top: "35px",
+              left: "460px",
+            }}
+          />
+          <Box
+            sx={{
+              position: "relative",
+              backgroundImage: `url(${reviewBlockAvatar4})`,
+              backgroundSize: "cover",
+              height: "65px",
+              width: "65px",
+              borderRadius: "30px",
+              top: "85px",
+              left: "100px",
+            }}
+          />
+          <Box
+            sx={{
+              position: "relative",
+              backgroundImage: `url(${reviewBlockAvatar5})`,
+              backgroundSize: "cover",
+              height: "65px",
+              width: "65px",
+              borderRadius: "30px",
+              left: "350px",
+              top: "60px",
+            }}
+          />
+          <Box
+            sx={{
+              position: "relative",
+              backgroundImage: `url(${reviewBlockAvatar6})`,
+              backgroundSize: "cover",
+              height: "45px",
+              width: "45px",
+              borderRadius: "20px",
+              top: " 70px",
+              left: "50px",
+            }}
+          />
+          <Box
+            sx={{
+              position: "relative",
+              backgroundImage: `url(${reviewBlockAvatar7})`,
+              backgroundSize: "cover",
+              height: "45px",
+              width: "45px",
+              borderRadius: "20px",
+              bottom: "-183px",
+            }}
+          />
+          <Box
+            sx={{
+              position: "relative",
+              backgroundImage: `url(${reviewBlockAvatar8})`,
+              backgroundSize: "cover",
+              height: "65px",
+              width: "65px",
+              borderRadius: "30px",
+              left: "250px",
+            }}
+          />
+        </Box>
+      </Box>
     </ReviewsWrapper>
   );
 };
