306969c56d1b1c53d80a8322d016ce1802a26527
Cristian <ustianschii@gmail.com>
2024-06-08 12:24:20 +0300

    test2

--- src/components/OurWaranty/OurWaranty.tsx
+++ src/components/OurWaranty/OurWaranty.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Box, Typography } from "@mui/material";
 import { CardsContainer, OurWarantyWrapper } from "./ourWarantyStyles";
 import { WarantyCard } from "./WarantyCard/WarantyCard";
